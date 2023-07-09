56c54446d4c071194a262aa24b1bee4472b7e540
Kjell-Arne(Sovak) <kjell-arne@neshagen.no>
2023-07-09 20:42:15 +0200

    Last commit before delivery

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2,5 +2,9 @@ const menuIcon = document.querySelector(".mobile-menu-icon");
 const navbarMenu = document.querySelector('.navbar-menu');
 
 menuIcon.addEventListener('click', () => {
-
+  if(navbarMenu.classList.contains("show-menu")) {
+  navbarMenu.classList.remove("show-menu");
+  } else {
+    navbarMenu.classList.add("show-menu");
+  }
 });
